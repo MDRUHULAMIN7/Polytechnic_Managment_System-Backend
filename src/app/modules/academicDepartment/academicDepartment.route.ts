f91afdc8acaf3c0a5408cf5b18e33c7b4075893d
mdruhulamin7 <ruhulthisis@gmail.com>
2026-01-22 10:49:29 +0600

    Create Academic Department interface , model , validation ,service

---
+++ src/app/modules/academicDepartment/academicDepartment.route.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import validateRequest from '../../middleware/validateRequest.js';
+import { AcademicDepartmentValidation } from './academicDepartment.validation.js';
+import { AcademicDepartmentControllers } from './academicDepartment.controller.js';
+
+const router = express.Router();
+
+router.post(
+  '/create-academic-department',
+  validateRequest(
+    AcademicDepartmentValidation.createAcademicDepartmentValidationSchema,
+  ),
+  AcademicDepartmentControllers.createAcademicDepartmemt,
+);
+
+router.get(
+  '/:departmentId',
+  AcademicDepartmentControllers.getSingleAcademicDepartment,
+);
+
+router.patch(
+  '/:departmentId',
+  validateRequest(
+    AcademicDepartmentValidation.updateAcademicDepartmentValidationSchema,
+  ),
+  AcademicDepartmentControllers.updateAcademicDeartment,
+);
+
+router.get('/', AcademicDepartmentControllers.getAllAcademicDepartments);
+
+export const AcademicDepartmentRoutes = router;
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,6 +3,7 @@ import { UserRoutes } from "../modules/user/user.route.js";
 import { StudentRoutes } from "../modules/student/student.route.js";
 import { AcademicSemsterRoutes } from "../modules/academicSemester/academicSemester.routes.js";
 import { AcademicInstructorRoutes } from "../modules/academicInstructor/academicInstructor.route.js";
+import { AcademicDepartmentRoutes } from "../modules/academicDepartment/academicDepartment.route.js";
 
 
 const router = Router()
@@ -23,6 +24,10 @@ const moduleRoutes = [
     {
         path:"/academic-instructor",
         route:AcademicInstructorRoutes
+    },
+    {
+        path:"/academic-department",
+        route:AcademicDepartmentRoutes
     },
 ]
          moduleRoutes.forEach(route => router.use(route.path,route.route))
