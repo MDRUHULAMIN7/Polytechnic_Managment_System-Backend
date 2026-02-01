3ceb09445d96503c806cb0f7edffb000cabb4f0c
mdruhulamin7 <ruhulthisis@gmail.com>
2026-02-01 19:45:54 +0600

    Understanding Error Patterns in Zod

--- src/app/middleware/globalErrorHandeler.ts
+++ src/app/middleware/globalErrorHandeler.ts
@@ -1,15 +1,30 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import type { NextFunction, Request, Response } from "express";
+import type { ErrorRequestHandler, NextFunction } from "express";
+import { ZodError } from "zod";
 
-const globalErrorHandeler =((err:any,req:Request,res:Response,next:NextFunction)=>{
+const globalErrorHandeler : ErrorRequestHandler =((err,req,res,next:NextFunction)=>{
 
-  const statusCode = 500;
-  const message = err.message || 'something went wrong' ;
+  let statusCode = 500;
+  let message = err.message || 'something went wrong' ;
 
+  type TErrorSources={
+    path:string | number;
+    message:string
+  }[];
+  let errorSources : TErrorSources = [{
+    path:'',
+    message:"Something went Wrong",
+  }]
+  
+   if(err instanceof ZodError){
+    statusCode = 400;
+    message = 'ami zod error';
+   }
   return res.status(statusCode).json({
     success:false,
     message,
-    error:err
+    errorSources,
+    error:err,
   })
 
 })
