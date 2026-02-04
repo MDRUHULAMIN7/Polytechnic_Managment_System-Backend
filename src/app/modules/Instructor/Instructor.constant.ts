88ac4c9e547ac6e414ebf3bb5d4dac5ee12522ab
mdruhulamin7 <ruhulthisis@gmail.com>
2026-02-04 14:36:29 +0600

    Instructor Interface & model created

---
+++ src/app/modules/Instructor/Instructor.constant.ts
@@ -0,0 +1,24 @@
+import type { TBloodGroup, TGender } from "./Instructor.interface.js";
+
+export const Gender: TGender[] = ['male', 'female', 'other'];
+
+export const BloodGroup: TBloodGroup[] = [
+  'A+',
+  'A-',
+  'B+',
+  'B-',
+  'AB+',
+  'AB-',
+  'O+',
+  'O-',
+];
+
+export const InstructorSearchableFields = [
+  'email',
+  'id',
+  'contactNo',
+  'emergencyContactNo',
+  'name.firstName',
+  'name.lastName',
+  'name.middleName',
+];
--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,65 +1,87 @@
-
 import mongoose from 'mongoose';
 import type { TStudent } from './student.interface.js';
 import { Student } from './student.model.js';
 import AppError from '../../errors/AppError.js';
 import { StatusCodes } from 'http-status-codes';
 import { User } from '../user/user.model.js';
+import QueryBuilder from '../../../builder/QueryBuilder.js';
+import { studentSearchableFields } from './student.constant.js';
 
 
 const getAllStudentFromDB = async (query: Record<string, unknown>) => {
-  const queryObj = { ...query };
-
-  const studentSearchableFields = [
-    'email',
-    'name.firstName',
-    'presentAddress',
-  ];
-
-  //Search
-  if (queryObj.searchTerm) {
-    const searchTerm = queryObj.searchTerm as string;
-
-    queryObj.$or = studentSearchableFields.map(field => ({
-      [field]: { $regex: searchTerm, $options: 'i' },
-    }));
-  }
-
-  // Remove non-filter fields
-  const excludeFields = ['searchTerm', 'sort', 'page', 'limit','fields'];
-  excludeFields.forEach(field => delete queryObj[field]);
-
-  //  Base query
-  let mongooseQuery = Student.find(queryObj)
-    .populate({
-      path: 'academicDepartment',
-      populate: {
-        path: 'academicInstructor',
-      },
-    })
-    .populate('admissionSemester');
-
-  // Sorting -> Sort by createdAt DESC, break ties with _id DESC to ensure stable paginatio
-  const sort = (query.sort as string) || '-createdAt -_id';
-  mongooseQuery = mongooseQuery.sort(sort);
-
-  // Pagination
-  const page = Number(query.page) || 1;
-  const limit = Number(query.limit) || 10;
-  const skip = (page - 1) * limit;
-
-  mongooseQuery = mongooseQuery.skip(skip).limit(limit);
-
-  // Execute
-
-  // feilds limiting
-  let feilds = "-_v";
-
-  if(query.feilds){
-    feilds = (query.feilds as string).split(',').join(' ');
-  }
  
- const result  = await mongooseQuery.select(feilds);
+ 
+//   const queryObj = { ...query };
+
+//   const studentSearchableFields = [
+//     'email',
+//     'name.firstName',
+//     'presentAddress',
+//   ];
+
+//   //Search
+//   if (queryObj.searchTerm) {
+//     const searchTerm = queryObj.searchTerm as string;
+
+//     queryObj.$or = studentSearchableFields.map(field => ({
+//       [field]: { $regex: searchTerm, $options: 'i' },
+//     }));
+//   }
+
+//   // Remove non-filter fields
+//   const excludeFields = ['searchTerm', 'sort', 'page', 'limit','fields'];
+//   excludeFields.forEach(field => delete queryObj[field]);
+
+//   //  Base query
+//   let mongooseQuery = Student.find(queryObj)
+//     .populate({
+//       path: 'academicDepartment',
+//       populate: {
+//         path: 'academicInstructor',
+//       },
+//     })
+//     .populate('admissionSemester');
+
+//   // Sorting -> Sort by createdAt DESC, break ties with _id DESC to ensure stable paginatio
+//   const sort = (query.sort as string) || '-createdAt -_id';
+//   mongooseQuery = mongooseQuery.sort(sort);
+
+//   // Pagination
+//   const page = Number(query.page) || 1;
+//   const limit = Number(query.limit) || 10;
+//   const skip = (page - 1) * limit;
+
+//   mongooseQuery = mongooseQuery.skip(skip).limit(limit);
+
+//   // Execute
+
+//   // feilds limiting
+//   let feilds = "-_v";
+
+//   if(query.feilds){
+//     feilds = (query.feilds as string).split(',').join(' ');
+//   }
+ 
+//  const result  = await mongooseQuery.select(feilds);
+//   return result;
+  const studentQuery = new QueryBuilder(
+    Student.find()
+      .populate('admissionSemester')
+      .populate({
+        path: 'academicDepartment',
+        populate: {
+          path: 'academicInstructor',
+        },
+      }),
+    query,
+  )
+    .search(studentSearchableFields)
+    .filter()
+    .sort()
+    .paginate()
+    .fields();
+
+  const result = await studentQuery.modelQuery;
   return result;
 };
 
